387a574bab4941f6f7a7d908c97bd9150e177774
Tony-123-a11y <ayushsinghatschool@gmail.com>
2025-09-02 18:31:56 +0530

    turn testing

--- Frontend/src/context/PeerContext.jsx
+++ Frontend/src/context/PeerContext.jsx
@@ -17,7 +17,16 @@ export const PeerProvider=({children})=>{
                     "stun:stun1.l.google.com:19302",
                  
                 ]
-            }
+            },
+            {
+      urls: [
+        "turn:openrelay.metered.ca:80",
+        "turn:openrelay.metered.ca:443",
+        "turn:openrelay.metered.ca:443?transport=tcp"
+      ],
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
         ]
      })
    },[])
