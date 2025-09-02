5999a321f25cccb43eb9810bd292082d3006c8e1
Tony-123-a11y <ayushsinghatschool@gmail.com>
2025-09-02 21:19:56 +0530

    backendurlupdate

--- Frontend/src/context/PeerContext.jsx
+++ Frontend/src/context/PeerContext.jsx
@@ -12,7 +12,7 @@ export const PeerProvider=({children})=>{
     console.log(iceServerURLs)
     async function getIceServers(){
        try {
-         const res= await fetch('http://localhost:8000/ice-servers')
+         const res= await fetch('https://talktrackr-2.onrender.com/ice-servers')
          const data= await res.json()
          console.log(data)
         setIceServerURLs(data.token.iceServers)
